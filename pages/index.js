c1d4c42241a4025196e141a8b96efb0f8d209843
Zac Cooper <zacooper5g@outlook.com>
2023-01-09 09:29:41 +1100

    began adding nav component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
+import Head from "next/head";
+import Header from "../components/Header";
+import Nav from "../components/Nav";
+
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Hulu V2</title>
+      </Head>
+      <Header />
+      <Nav />
     </>
   )
-}
+};
