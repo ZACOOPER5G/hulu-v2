c0f524986426e51d18f40e5702d9b97cfc46f6f1
Zac Cooper <zacooper5g@outlook.com>
2023-01-10 09:38:41 +1100

    updated styles

--- components/Nav.js
+++ components/Nav.js
@@ -10,15 +10,15 @@ const Nav = () => {
 				{Object.entries(requests).map(([key, { title, url }]) => (
 					<h2
 						key={key}
-                        onClick={() => router.push(`/?genre=${key}`)}
+						onClick={() => router.push(`/?genre=${key}`)}
 						className="last:pr-12 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500"
 					>
 						{title}
 					</h2>
 				))}
 			</div>
-			<div className="absolute top-0 left-0 bg-gradient-to-r from-[#06202A] h-10 w-1/12" />
-			<div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12" />
+			<div className="absolute top-0 left-0 bg-gradient-to-r from-[#06202A] h-10 w-1/12 pointer-events-none" />
+			<div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12 pointer-events-none" />
 		</nav>
 	);
 };
