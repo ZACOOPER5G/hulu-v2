519d31fccbaf689a4c71c7ce15f563b3466ca83b
Zac Cooper <zacooper5g@outlook.com>
2023-01-10 10:03:12 +1100

    added loading animation

--- components/Results.js
+++ components/Results.js
@@ -1,11 +1,12 @@
 import Thumbnail from "./Thumbnail";
+import FlipMove from "react-flip-move";
 
 const Results = ({ results }) => {
-	return <div className="px-5 my-10 sm:grid md:grid-cols-2 lg:grid-cols-3 2xl:flex flex-wrap justify-center">
+	return <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 lg:grid-cols-3 2xl:flex flex-wrap justify-center">
         {results.map(result => (
             <Thumbnail key={result.id} result={result} />
         ))}
-    </div>;
+    </FlipMove>;
 };
 
 export default Results;
--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,10 +1,13 @@
 import { ThumbUpIcon } from "@heroicons/react/outline";
 import Image from "next/image";
-import React from "react";
+import { forwardRef } from "react";
 
-const Thumbnail = ({ result }) => {
+const Thumbnail = forwardRef(({ result }, ref) => {
 	return (
-		<div className="group cursor-pointer p-2 transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50 max-w-md">
+		<div
+			ref={ref}
+			className="group cursor-pointer p-2 transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50 max-w-md"
+		>
 			<Image
 				layout="responsive"
 				src={
@@ -13,11 +16,17 @@ const Thumbnail = ({ result }) => {
 				}
 				width={1920}
 				height={1080}
-				alt={ result.backdrop_path === null || result.poster_path === null ? "Film poster not available" : "film poster" }
+				alt={
+					result.backdrop_path === null || result.poster_path === null
+						? "Film poster not available"
+						: "film poster"
+				}
 			/>
 			<div className="p-2">
 				<p className="truncate max-w-md">{result.overview}</p>
-				<h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">{result.title}</h2>
+				<h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">
+					{result.title}
+				</h2>
 				<p className="flex items-center opacity-0 group-hover:opacity-100">
 					{result.media_type && `${result.media_type} • `}
 					{result.release_date || result.first_air_date} •
@@ -26,6 +35,6 @@ const Thumbnail = ({ result }) => {
 			</div>
 		</div>
 	);
-};
+});
 
 export default Thumbnail;
