42e49a9816266dc95f4034e91b2276d25f39b8fa
Zac Cooper <zacooper5g@outlook.com>
2023-01-09 14:27:18 +1100

    added server-side props

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,34 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
+import Results from "../components/Results";
+import requests from "../utils/requests";
 
-export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Hulu V2</title>
-      </Head>
-      <Header />
-      <Nav />
-    </>
-  )
-};
+export default function Home({ results }) {
+	return (
+		<>
+			<Head>
+				<title>Hulu V2</title>
+			</Head>
+			<Header />
+			<Nav />
+			<Results results={results} />
+		</>
+	);
+}
+
+export async function getServerSideProps(context) {
+	const genre = context.query.genre;
+	const request = await fetch(
+		`https://api.themoviedb.org/3${
+			requests[genre]?.url || request.fetchTrending.url
+		}`
+	);
+	const data = await request.json();
+
+	return {
+		props: {
+			results: data.results,
+		},
+	};
+}
